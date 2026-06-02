1fd37c6516cbb9106868eb5b92ed35cf5afb1765
KipletingEdwin <kipletingedwin4@gmail.com>
2026-06-02 08:53:22 +0100

    commit

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,7 @@
 
 import { Button } from "@/components/Button"
-import { Menu } from "lucide"
-import { MenuIcon } from "lucide-react"
+import { MenuIcon, X } from "lucide-react"
+import { useState } from "react"
 
 const navLinks = [
   { href: "#about", label: "About"},
@@ -12,6 +12,10 @@ const navLinks = [
 
 
 export const Navbar = () => {
+  const[isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+
+
   return (
     <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
       <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -43,13 +47,16 @@ export const Navbar = () => {
         </div>
 
         {/* Mobile Menu Button */}
-        <button className="md:hidden p-2 text-foreground">
-          <MenuIcon size={24}/>
+        <button className="md:hidden p-2 text-foreground cursor-pointer" 
+        onClick={() => setIsMobileMenuOpen((prev) => !prev)}   >
+          { isMobileMenuOpen ? <X/> : <MenuIcon size={24}/>}
         </button>
       </nav>
 
       {/* Mobile Menu */}
-      <div className="md:hidden glass-strong">
+
+      { isMobileMenuOpen && (
+        <div className="md:hidden glass-strong animate-fade-in">
         <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
             {
               navLinks.map((link, index) => (
@@ -59,10 +66,10 @@ export const Navbar = () => {
                 className="text-lg text-muted-foreground hover:text-foreground py-2"
                 >{link.label}</a>
               ))}
-              
               <Button size="sm">Contact Me</Button>
         </div>
       </div>
+       )}
 
 
     </header>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,6 +2,17 @@
 
 export const Hero = () => {
   return (
-    <div> </div>
+    <section className="relative min-h-screen flex items-center overflow-hidden"> 
+    {/* Bg */}
+
+    <div className="absolute inset-0">
+      <img src="/hero-bg.jpg" 
+      alt="Hero image" 
+      className="w-full h-full object-cover opacity-40"
+      />
+      <div className="absolute" />
+    </div>
+
+    </section>
   )
 }
