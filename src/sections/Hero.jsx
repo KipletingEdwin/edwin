f609ee4bad67dfdc7279f6c9461fa59c5ebed5fd
KipletingEdwin <kipletingedwin4@gmail.com>
2026-06-02 09:47:21 +0100

    commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,7 +10,20 @@ export const Hero = () => {
       alt="Hero image" 
       className="w-full h-full object-cover opacity-40"
       />
-      <div className="absolute" />
+      <div className="absolute inset-0 bg-linear-to-b from-background/20 via-background/80 to-background    "/>
+    </div>
+
+    {/* Green Dots */}
+    <div>
+      {[...Array(30)].map((_, i) => (
+        <div className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+        style={{
+          backgroundColor: "#20B2A6",
+        }}
+        />
+      ))
+
+      }
     </div>
 
     </section>
